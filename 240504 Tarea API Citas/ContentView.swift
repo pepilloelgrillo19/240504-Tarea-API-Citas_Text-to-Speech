12bca2ab6dc849f9db779179ef3561d6209833b3
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-05-04 19:29:37 +0200

    Initial Commit

---
+++ 240504 Tarea API Citas/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  240504 Tarea API Citas
+//
+//  Created by Luis on 4/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 240504 Tarea API Citas/_40504_Tarea_API_CitasApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40504_Tarea_API_CitasApp.swift
+//  240504 Tarea API Citas
+//
+//  Created by Luis on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct _40504_Tarea_API_CitasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
